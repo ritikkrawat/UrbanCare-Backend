f2920188c15481277c334a5fa48d960dba4511d3
Ritik Rawat <rithikrawat2004@gmail.com>
2026-02-02 23:27:23 +0530

    Update authController.jsx

--- urbancarebackend/src/controllers/authController.jsx
+++ urbancarebackend/src/controllers/authController.jsx
@@ -1,6 +1,8 @@
 const User = require("../models/user.jsx");
 const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken.jsx")
 
+// REGISTER USER
 const registerUser = async (req, res) => {
   try {
     const { name, email, mobile, password } = req.body;
@@ -43,4 +45,59 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const { identifier, password } = req.body;
+
+    if (!identifier || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      });
+    }
+
+    const user = await User.findOne({
+      $or: [{ email: identifier }, { mobile: identifier }]
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        mobile: user.mobile
+      }
+    });
+  } catch (error) {
+    console.error("LOGIN ERROR:", error.message);
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+
+module.exports = { registerUser, loginUser };
