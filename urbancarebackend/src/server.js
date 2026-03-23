74dea0b8a4acd713d70b7f518f48fa580076d7b4
Ritik Rawat <rithikrawat2004@gmail.com>
2026-03-23 15:09:00 +0530

    Create server.js

---
+++ urbancarebackend/src/server.js
@@ -0,0 +1,14 @@
+const app = require("./app.js");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+
+dotenv.config();
+
+// CONNECT DATABASE
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`UrbanCare Backend running on port ${PORT}`);
+});
