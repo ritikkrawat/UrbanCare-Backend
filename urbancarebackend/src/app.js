050f4b9cb4289df41fc4071b67627ebd6df2b7d0
Ritik Rawat <rithikrawat2004@gmail.com>
2026-03-23 15:08:03 +0530

    Create app.js

---
+++ urbancarebackend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes.js");
+const userRoutes = require("./routes/userRoutes.js");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("UrbanCare Backend is running");
+});
+
+module.exports = app;
