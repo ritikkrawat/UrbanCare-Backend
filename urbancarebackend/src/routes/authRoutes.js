049cef0e043398cfce77003d93805df0dd6f9742
Ritik Rawat <rithikrawat2004@gmail.com>
2026-04-02 13:49:09 +0530

    Update authRoutes.js

--- urbancarebackend/src/routes/authRoutes.js
+++ urbancarebackend/src/routes/authRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
+
 const {
   registerUser,
-  loginUser
+  loginUser,
+  deleteInstant,
+  deleteRequest,
+  changePassword
 } = require("../controllers/authController.js");
+
+const protect = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.delete("/delete-instant", protect, deleteInstant); 
+router.post("/delete-request", protect, deleteRequest);
+router.put("/change-password", protect, changePassword);
+
+console.log("Auth Routes loaded");
 
 module.exports = router;
