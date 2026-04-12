b9d970fc5a46bc9a5cedbac692ab0747818ad16d
Ritik Rawat <rithikrawat2004@gmail.com>
2026-04-12 22:48:19 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const app = require("../src/app");
+const dotenv = require("dotenv");
+const connectDB = require("../src/config/db");
+
+dotenv.config();
+
+// Cache DB connection (important for serverless)
+let isConnected = false;
+
+async function connect() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+}
+
+module.exports = async (req, res) => {
+  await connect();
+  return app(req, res);
+};
