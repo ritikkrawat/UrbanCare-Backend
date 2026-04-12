232433fbf521a0d25f802ba03b4404cd6cd3dfab
Ritik Rawat <rithikrawat2004@gmail.com>
2026-04-12 22:48:27 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
   }
 };
 
