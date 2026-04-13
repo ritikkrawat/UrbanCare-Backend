850d80ef7ae032e2b2013001e2dad3d9ef55e687
Ritik Rawat <rithikrawat2004@gmail.com>
2026-04-13 20:23:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,16 +4,29 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const complaintRoutes = require("./routes/complaintRoutes.js");
-
+const path = require("path");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://urbancaredev.vercel.app",
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/complaint", complaintRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: err.message || "Server Error"
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("UrbanCare Backend is running");
