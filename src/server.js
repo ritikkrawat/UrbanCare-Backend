0bf9df8232de32d06dbbf92ca086881533104710
Ritik Rawat <rithikrawat2004@gmail.com>
2026-04-13 20:30:09 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,12 +3,6 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 
 dotenv.config();
-
-// CONNECT DATABASE
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`UrbanCare Backend running on port ${PORT}`);
-});
+module.exports = app;
